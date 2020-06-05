c8859aa474c5ca44297ce6e89f3f6b292ab991ea
PetrAdminus <43944943+PetrAdminus@users.noreply.github.com>
2020-06-05 22:34:08 +0500

    Ready

---
+++ script/script.js
@@ -0,0 +1,8 @@
+//  Объявляем переменные
+let money, income, addExpenses, deposit, mission, period;
+
+// Выводим модальное окно с сообщением 
+alert("Мой первый alert");
+
+// Выводим сообщение в консоли
+console.log("Мой первый console.log");
