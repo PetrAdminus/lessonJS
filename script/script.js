c53f5523f1b54733ffcb2b14ef6a782b2c7442e8
PetrAdminus <43944943+PetrAdminus@users.noreply.github.com>
2020-06-06 18:35:32 +0500

    lesson02

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
 //  Объявляем переменные
-let money, income, addExpenses, deposit, mission, period;
+let money = 100000,
+  income = "Верстка",
+  addExpenses = "Интернет, Такси, Коммуналка",
+  deposit = true,
+  mission = 1000000,
+  period = 12;
 
-// Выводим модальное окно с сообщением 
-alert("Мой первый alert");
+// Выводим в консоль тип данных зачений переменных
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-// Выводим сообщение в консоли
-console.log("Мой первый console.log");
+// Выводим в консоль длину строки
+console.log(addExpenses.length);
+
+// Выводим сообщение c переменной 
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+// Выводим строку в нижнем регистре + массив
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+// Объявляем новую переменную
+let budgetDay = money / 30;
+console.log(budgetDay);
