ed6efe512b2c982748cd82c21f09899478db044b
PetrAdminus <43944943+PetrAdminus@users.noreply.github.com>
2020-06-09 01:09:47 +0500

    lesson03 ready

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
+'use sctrict'
 //  Объявляем переменные
-let money = 100000,
+let money = +prompt("Ваш месячный доход?"),
   income = "Верстка",
-  addExpenses = "Интернет, Такси, Коммуналка",
-  deposit = true,
+  addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
+  deposit = confirm("Есть ли у вас депозит в банке?"),
   mission = 1000000,
   period = 12;
 
@@ -22,6 +23,30 @@ console.log("Цель заработать " + mission + " рублей");
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 
-// Объявляем новую переменную
-let budgetDay = money / 30;
-console.log(budgetDay);
+let expenses1 = prompt("Введите обязательную статью расходов:"),
+  amount1 = +prompt("Во сколько это обойдется?"),
+  expenses2 = prompt("Введите обязательную статью расходов:"),
+  amount2 = +prompt("Во сколько это обойдется?");
+
+let budgetMonth = money - (amount1 + amount2);
+console.log("Бюджет на месяц " + budgetMonth);
+
+console.log("Цель будет достигнута, через " + Math.ceil(mission / budgetMonth) + " месяцев");
+
+let budgetDay = budgetMonth / 30;
+console.log("Бюджет на день " + budgetDay);
+
+switch (true) {
+  case budgetDay >= 1200:
+    console.log("У вас высокий уровень дохода");
+    break;
+  case budgetDay < 1200 && budgetDay >= 600:
+    console.log("У вас средний уровень дохода");
+    break;
+  case budgetDay < 600 && budgetDay > 0:
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+    break;
+  case budgetDay < 0:
+    console.log("Что то пошло не так");
+    break;
+}
